afc1c3e57a660d4b0869dff3317d832016c9975d
itobuka <itobuka2019-10@ftn.edu.rs>
2020-10-22 19:32:49 +0200

    restructuring for landing page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react"
 import { BrowserRouter } from "react-router-dom"
-import Header from "./components/Header"
-import Main from "./components/Main"
+/* import Header from "./components/Header"
+import Main from "./components/Main" */
+import Home from "./pages/Home"
 
 function App() {
   return (
     <BrowserRouter>
       <div className='app'>
-        <Header />
-        <Main />
+        <Home />
+        {/* <Header />
+        <Main /> */}
         {/* <Footer /> */}
       </div>
     </BrowserRouter>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,14 +5,14 @@ import cheapSheep from "./../assets/images/cheapSheep.svg"
 function Home() {
   return (
     <div className='home'>
-      <header className='home-header'>
-        <img src={cheapSheep} className='home-logo' alt='logo' />
-        <h1>
-          <NavLink to='/lessons' className='home-link'>
-            React Cheap Sheep
-          </NavLink>
-        </h1>
-      </header>
+      <h1>Cheap Sheep</h1>
+      <p>Cheap Sheep general description . . .</p>
+      {/* <img src={cheapSheep} className='home-logo' alt='logo' /> */}
+      <h2>
+        <NavLink to='/lessons' className='home-link'>
+          <img src={cheapSheep} className='home-logo' alt='logo' />
+        </NavLink>
+      </h2>
     </div>
   )
 }
